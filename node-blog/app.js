7900b3abe7d5c5735fa9546035f411b68448258a
teeaaspoon <tommypoon36@gmail.com>
2018-06-10 22:17:35 +0000

    completed app.js

---
+++ node-blog/app.js
@@ -0,0 +1,59 @@
+/* app.js */
+
+// require and instantiate express
+var app = require("express")();
+
+// fake posts to simulate a database
+var posts = [
+    {
+        id: 1,
+        author: "John",
+        title: "Templating with EJS",
+        body: "Blog post number 1"
+    },
+    {
+        id: 2,
+        author: "Drake",
+        title: "Express: Starting from the Bottom",
+        body: "Blog post number 2"
+    },
+    {
+        id: 3,
+        author: "Emma",
+        title: "Streams",
+        body: "Blog post number 3"
+    },
+    {
+        id: 4,
+        author: "Oceanlab",
+        title: "On A Good Day",
+        body: "Blog post number 4"
+    }
+];
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// blog homepage
+app.get("/", function(req, res) {
+    // render 'home.ejs' with the list of posts
+    res.render("home", { posts: posts });
+});
+
+// blog post
+app.get("/post/:id", function(req, res) {
+    //find the post in the 'posts' array
+    var post = posts.filter(function(post) {
+        return post.id == req.params.id;
+    })[0];
+    // render the 'post.ejs' template with the post content
+    res.render("post", {
+        author: post.author,
+        title: post.title,
+        bodt: post.body
+    });
+});
+
+app.listen(8080);
+
+console.log("listening on port 8080");
